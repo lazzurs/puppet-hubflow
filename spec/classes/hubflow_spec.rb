a50daa69cdcbe2dabe9489c1e2ea5826eaf9d9f1
Rob Lazzurs <r.lazzurs@kainos.com>
2013-11-19 23:53:10 +0000

    Initial README fixes, test fixes should now pass.
    Initial README fixes, but this still needs to be finished but
    at least the travis-ci link will work.
    
    Tests now pass locally but need to see if they will pass on the
    build server.

--- spec/classes/hubflow_spec.rb
+++ spec/classes/hubflow_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
+
 describe 'hubflow' do
+  let(:boxenhome) { '/opt/boxen' }
+  let(:repodir) { "#{boxenhome}/repo" }
+  let(:facts) do
+    {
+      :boxen_home    => boxenhome,
+      :boxen_repodir => repodir,
+    }
+  end
+
+
+
   it { should include_class('homebrew') }
 end
