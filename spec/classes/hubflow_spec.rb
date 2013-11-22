bb261e5ff831bcfb3b1ed20761c4bd89e8799610
Rob Lazzurs <rob@lazzurs.org>
2013-11-22 00:40:23 +0000

    Try changing ruby version back.
    To see if this fixes the spec issues.

--- spec/classes/hubflow_spec.rb
+++ spec/classes/hubflow_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 describe 'hubflow' do
-  let(:boxenhome) { '/opt/boxen' }
-  let(:repodir) { "#{boxenhome}/repo" }
-  let(:facts) do
-    {
-      :boxen_home    => boxenhome,
-      :boxen_repodir => repodir,
-    }
+  #let(:boxenhome) { '/opt/boxen' }
+  #let(:repodir) { "#{boxenhome}/repo" }
+  #let(:facts) do
+  #  {
+  #    :boxen_home    => boxenhome,
+  #    :boxen_repodir => repodir,
+  #  }
+  #end
+
+  it do
+    should include_class('homebrew')
+    should contain_homebrew__formula('hub-flow')
+    should contain_package('hubflow').with_ensure('1.5.2')
   end
-
-  it { should include_class('homebrew') }
-
-  should contain_homebrew__formula('hub-flow')
-
-  should contain_package('hubflow').with_ensure('1.5.2')
-
 end
