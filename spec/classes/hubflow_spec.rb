a5b32c257facd7091590ce38478ebcf2c409e7c1
Rob Lazzurs <rob@lazzurs.org>
2013-11-23 09:14:02 +0000

    Adding the required module directory and reactivating defines in spec.
    Should have noticed this directory was named template and needed
    to be renamed for my module however maybe this should be in the
    documentation or scripts.
    
    Had to reactivate the defines in the spec file.  Now passing tests.

--- spec/classes/hubflow_spec.rb
+++ spec/classes/hubflow_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe 'hubflow' do
-  #let(:boxenhome) { '/opt/boxen' }
-  #let(:repodir) { "#{boxenhome}/repo" }
-  #let(:facts) do
-  #  {
-  #    :boxen_home    => boxenhome,
-  #    :boxen_repodir => repodir,
-  #  }
-  #end
+  let(:boxenhome) { '/opt/boxen' }
+  let(:repodir) { "#{boxenhome}/repo" }
+  let(:facts) do
+    {
+      :boxen_home    => boxenhome,
+      :boxen_repodir => repodir,
+    }
+  end
 
   it do
     should include_class('homebrew')
